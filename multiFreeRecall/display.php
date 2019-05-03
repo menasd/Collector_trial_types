528b317b5e27fd08b3f3c63c08a0dd7a1b9b9f2a
menasd <48842545+menasd@users.noreply.github.com>
2019-05-03 14:42:56 -0700

    Create display.php

---
+++ multiFreeRecall/display.php
@@ -0,0 +1,76 @@
+<!-- AUTHOR COLLABORATION: Girija Chantufale -->
+
+<!---Question 1 -->
+<div>
+    <div class="textarea-label shim">
+    	<?php 
+    		echo $text;
+    		$question1 = "SHOE:";
+    		echo $question1; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Respons1" wrap="physical" value=""></textarea>
+</div>
+  
+  <!---Question 2 -->
+<div>
+        <div class="textarea-label shim">
+    	<?php 
+    		$question2 = "AUTOMOBILE TIRE:";
+    		echo $question2; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Response2" wrap="physical" value=""></textarea>
+</div>
+
+<!---Question 3 -->
+<div>
+        <div class="textarea-label shim">
+    	<?php 
+    		$question3 = "NEWSPAPER:";
+    		echo $question3; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Response3" wrap="physical" value=""></textarea>
+</div>
+
+<!---Question 4 -->
+<div>
+        <div class="textarea-label shim">
+    	<?php 
+    		$question4 = "BUTTON:";
+    		echo $question4; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Response4" wrap="physical" value=""></textarea>
+</div>
+
+<!---Question 5 -->
+<div>
+        <div class="textarea-label shim">
+    	<?php 
+    		$question5 = "PENCIL:";
+    		echo $question5; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Response5" wrap="physical" value=""></textarea>
+</div>
+
+<!---Question 6 -->
+<div>
+        <div class="textarea-label shim">
+    	<?php 
+    		$question6 = "CORK:";
+    		echo $question6; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Response6" wrap="physical" value=""></textarea>
+</div>
+
+<!---Question 7 -->
+<div>
+        <div class="textarea-label shim">
+    	<?php 
+    		$question7 = "PAPER CUP:";
+    		echo $question7; ?>
+    	</div>
+    <textarea class="fr-textarea collectorInput" name="Response7" wrap="physical" value=""></textarea>
+</div>
+
+<div class="textright">
+    <button class="collectorButton collectorAdvance" id="FormSubmitButton">Submit</button>
+</div>
